5f8eb1cd4a21aca36bc72c9577c28fe6c7d11ae0
CherryProgger <alexander.korshunoff@gmail.com>
2023-11-13 23:45:04 +0300

    Home work 2 finished

--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
+
 questionsCount = 20
 questionsCountOnOnePage = 10
+
+
 QUESTIONS = [
     {
         'id': i,
         'title': f'Question {i}',
-        'content': f'A standard guitar has six strings made of various materials such as nylon, steel, or a combination of both (classical guitars, for example, would typically use nylon). The top three strings, also known as the treble strings, are typically made of plain steel, while the bottom three strings, also known as the bass strings, are made of a steel core wrapped in a thin layer of metal such as bronze or nickel. The thickness of each string determines its pitch. {i}'
+        'content': f'A standard guitar has six strings made of various materials such as nylon, steel, or a combination of both (classical guitars, for example, would typically use nylon). The top three strings, also known as the treble strings, are typically made of plain steel, while the bottom three strings, also known as the bass strings, are made of a steel core wrapped in a thin layer of metal such as bronze or nickel. The thickness of each string determines its pitch.'
     } for i in range(questionsCount)
 ]
+
 
 PAGES = [
     {
@@ -35,11 +39,13 @@ def addQuestion(request):
 
 def answersList(request, question_id):
     item = QUESTIONS[question_id]
-    return render(request, 'answersList.html', {'question': item})
+    page: object = request.GET.get('page', 1)
+    return render(request, 'answersList.html', {'question': item, 'questions': paginate(QUESTIONS, page), 'pages': PAGES})
 
 
 def tagQuestions(request):
-    return render(request, 'tagQuestions.html')
+    page: object = request.GET.get('page', 1)
+    return render(request, 'tagQuestions.html', {'questions': paginate(QUESTIONS, page), 'pages': PAGES})
 
 
 def settingsPage(request):
